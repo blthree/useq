bfc09fafd74d31be1d10c090e54edcaeefbbf40b
Benson Lindsey <blthree89@gmail.com>
2019-05-31 10:19:09 -0400

    start adding new components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import ReactDOM from "react-dom";
 import StepHolder from "./js/StepHolder.jsx"
-import NoteSelector from "./js/NoteSelector.jsx"
+// import NoteSelector from "./js/NoteSelector.jsx"
 
 // ReactDOM.render(<StepHolder num_notes={8}/>, document.getElementById('seq-output-holder'))
